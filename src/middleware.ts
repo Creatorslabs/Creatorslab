f8e6d41645d98794f5f7d4c18111d46bd31070c4
Enoch <arhyelphilip024@gmail.com>
2025-03-04 18:11:09 +0100

    checking images 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,10 @@ export function middleware(req: NextRequest) {
   // console.log("Waitlist middleware");
   // console.log(process.env.NODE_ENV);
 
-  if (process.env.NODE_ENV !== "development") {
+  if (
+    process.env.NODE_ENV !== "development" &&
+    req.nextUrl.pathname !== "/waitlist"
+  ) {
     return NextResponse.redirect(new URL("/waitlist", req.url));
   }
   return NextResponse.next();
@@ -18,10 +21,9 @@ export const config = {
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - public (public assets)
-     * - images (custom images directory)
+     * - public assets (image files explicitly excluded)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    "/((?!api|_next/static|_next/image|public|images|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(png|jpg|jpeg|gif|webp|svg|ico)).*)",
   ],
 };
