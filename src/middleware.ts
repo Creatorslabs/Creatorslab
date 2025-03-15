50a08fa624b76757651a207cf34547ead8fbc878
Enoch <arhyelphilip024@gmail.com>
2025-03-15 01:12:27 +0100

    hmm

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import type { NextRequest } from "next/server";
 
 export function middleware() {
   // Allow everything in development mode
